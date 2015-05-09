33a6e5d4eb1d82669de37a9dd0eb17dbd01b5005
Mesa TheMonk <elmesamola@gmail.com>
2015-05-09 11:34:49 +0200

    Debugging - Heroku deploy - Solving missing grunt-cli
    Changed dependency from "dev" to "standard"

--- web.js
+++ web.js
@@ -3,5 +3,7 @@ var express = require('express');
 var app = express();
 
 //app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+//app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use(express.static(__dirname + '/dist'));
+
 app.listen(process.env.PORT || 5000);
